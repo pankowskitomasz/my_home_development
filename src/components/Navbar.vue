19ac62fe98b8845d2a46bc28befea81edb8bb526
pankowskitomasz <pankowski_tomasz@o2.pl>
2020-12-08 19:25:32 +0100

    project's initial state

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="navbar navbar-dark navbar-expand-md bg-transparent position-absolute w-100">
+        <a href="/" class="navbar-brand font-logo font-weight-bold text-shadow">
+            <img src="img/navbar_logo.png" alt="logo">
+            Home Dev
+        </a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#main-nav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="main-nav">
+            <ul class="navbar-nav ml-auto text-shadow">
+                <li class="nav-item">
+                    <router-link to="/" class="nav-link font-weight-bold text-light">
+                        Home
+                    </router-link> 
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
---
+++ src/views/Error.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="error-s1 align-items-center d-flex">
+        <div class="row justify-content-center mx-0 w-100">
+            <div class="col-10 col-sm-6 text-center text-shadow">
+                <h2 class="font-weight-bold text-white">
+                    Page not found!
+                </h2>
+                <router-link to="/" class=" text-white">
+                    Back to <span class="font-weight-bold">Home</span>
+                </router-link>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default {
+  name: 'Error'
+}
+</script>
